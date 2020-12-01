b1943ab32683a8545abe5a4389cd6343d40aee4f
mmmiiiaaa <64200171+mmmiiiaaa@users.noreply.github.com>
2020-12-01 21:03:05 +0000

    individualArrows v4 done

--- individualArrows.py
+++ individualArrows.py
@@ -1,16 +1,19 @@
 from math import sqrt, pi, sin, cos
 import matplotlib.pyplot as plt
 
+
+list_ind=list()
 unit = 1  # refers to the unit length of each individual arrow - this is tbd
 height = 100  # refers to the y coordinate for the source and detector points (which will be directly opposite)
 
 
 # height will just be a set constant
 
-
+#parameters: frequency
 # point_x is the point where the light beam reflects off the horizontal surface (dependent on the value of n input)
 # source_x and detector_x are the x coordinates of the source and detector points
 # source_x I expect will be negative (on the left hand side of the origin) and detector_x positive, but this is tbd
+#output: the x and y coordinate for the rotated line
 def individual_arrows(frequency, source_x, detector_x, point_x):
     period = (1 / frequency)  # WILL HAVE TO ENSURE WHEN THE FREQUENCY IS INPUT IT IS KEPT IN UNIT Hz
     speed_light = 3.8e8
@@ -24,17 +27,21 @@ def individual_arrows(frequency, source_x, detector_x, point_x):
     return x, y
 
 
-
+#parameters: x and y (the output from individual_arrow)
+#output: the data for a matplotlib graph using the s and y coordinates
 def graph_ind_arrow(x, y, ):
+
     plt.plot([0, x], [0, y], color=(r, g,
                                     b))  # creates a graph that plots a line from 0,0 to x,y using the r g and b values
     # from the frequency_to_rgb procedure
     # this will need to be expanded and refined when I start creating the UI
 
+ind_out=(individual_arrows(frequency, source_x, detector_x, point_x))#ind_out=the list of the x and y coordinate
+list_ind.append(ind_out)
 
-# # __TEST AREA__
-# r = (int(
-#     input("input r"))) / 255  # formatplotlib rgb values should be from 0 to 1 so I will need to take the rgb values
+
+# #    __TEST AREA__
+# r = (int(input("input r"))) / 255  # formatplotlib rgb values should be from 0 to 1 so I will need to take the rgb values
 # # and divide by 255
 # g = (int(input("input g"))) / 255
 # b = (int(input("input b"))) / 255
@@ -42,8 +49,9 @@ def graph_ind_arrow(x, y, ):
 # detector_x = int(input("input detector_x"))
 # point_x = int(input("input point_x"))
 # frequency = float(input("input frequency"))
-# individual_arrows(frequency, source_x, detector_x, point_x)
-# # graph_ind_arrow(x,y)
-# print(x, y)
-individual_arrows(760000000000000, -50, 50, 0)
-print(x,y)
+#
+# graph_ind_arrow(ind_out[0],ind_out[1])
+# print(list_ind)
+# plt.show() #displays the graph_ind_arrow
+
+
